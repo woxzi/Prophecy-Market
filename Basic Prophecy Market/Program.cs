b4bd6756fa057cb89ea92a7fc49bbb5cb0bd9c66
Diego Vera <woxzii@gmail.com>
2019-09-16 11:53:58 -0400

    Project Creation

---
+++ Basic Prophecy Market/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_Prophecy_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
